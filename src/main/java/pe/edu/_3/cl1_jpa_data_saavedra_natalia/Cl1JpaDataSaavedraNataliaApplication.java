5063f1d06d11898b5401942c6ba92788d8a35f08
nataliaSaavedra <nataliadessyre729@gmail.com>
2024-11-20 03:03:14 -0500

    Initial Commit

---
+++ src/main/java/pe/edu/_3/cl1_jpa_data_saavedra_natalia/Cl1JpaDataSaavedraNataliaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu._3.cl1_jpa_data_saavedra_natalia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1JpaDataSaavedraNataliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1JpaDataSaavedraNataliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/_3/cl1_jpa_data_saavedra_natalia/Cl1JpaDataSaavedraNataliaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu._3.cl1_jpa_data_saavedra_natalia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1JpaDataSaavedraNataliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
