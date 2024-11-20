fd6bba3cee05814290df2b29e93864fc937a7d98
nataliaSaavedra <nataliadessyre729@gmail.com>
2024-11-20 03:14:44 -0500

    Método Run Implementado

--- src/main/java/pe/edu/_3/cl1_jpa_data_saavedra_natalia/Cl1JpaDataSaavedraNataliaApplication.java
+++ src/main/java/pe/edu/_3/cl1_jpa_data_saavedra_natalia/Cl1JpaDataSaavedraNataliaApplication.java
@@ -1,13 +1,18 @@
 package pe.edu._3.cl1_jpa_data_saavedra_natalia;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Cl1JpaDataSaavedraNataliaApplication {
+public class Cl1JpaDataSaavedraNataliaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cl1JpaDataSaavedraNataliaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
